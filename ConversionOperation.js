ffdd72f2ce560806a864108b559043d59c4009c5
rutuja <rutujakolte193@gmail.com>
2024-05-03 19:22:19 +0530

    ConversionOperation

---
+++ ConversionOperation.js
@@ -0,0 +1,6 @@
+let isLoggedIn = "Rutuja"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let Number = 50
+let stringNumber = String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
