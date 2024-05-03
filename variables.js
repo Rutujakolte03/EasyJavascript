15dfbbc421e073b1643885e40a4687f88c5448e9
rutuja <rutujakolte193@gmail.com>
2024-05-03 19:35:52 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId = 398896
+let accountEmail = "rutuja@google.com"
+var accountPassword = "1234"
+accountCity = "Pune"
+
+
+//accountId = 2//
+accountPassword = "5678"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table({accountId, accountEmail, accountPassword, accountCity})
