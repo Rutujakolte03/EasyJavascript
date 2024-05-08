c5aff3a3f2a1f32f84ed47944d037d15e4261df7
Rutuja <134671220+Rutujakolte03@users.noreply.github.com>
2024-05-08 23:01:52 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,27 @@
+"user strict"; //treat all JS code as newer version
+
+//alert( 3 + 3) //his function will display text in a dialog box that pops up on the screen.
+
+//code always be readable 
+//readability should be high
+console.log(3+3);
+console.log("Rutuja")
+
+let name = "rutuja" //sring
+
+let age = 21   //int
+
+let isLoggedIn = false   //boolean
+
+//number =>2 to power 53
+//biginit
+//string => ""
+//boolean => true / false
+//null => standalone value (empty)
+//undefined => 
+//symbol
+
+
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
